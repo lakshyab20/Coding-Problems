6dcdaf7a253b66a9e7b46cffb4ea1234dfca6b24
Lakshya Bhardwaj <47449860+lakshyab20@users.noreply.github.com>
2020-05-31 21:25:19 +0530

    Second Problem

---
+++ DailyProblem2.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class DailyProblem2
+{
+	public static void main(String args[])
+	{
+		int product=1;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the size of array: ");
+		int n=sc.nextInt();
+		int intArr[]=new int[n];
+		System.out.println("Enter the integers in array: ");
+		for(int i=0;i<n;i++){
+			intArr[i]=sc.nextInt();
+		}
+		for(int j=0;j<n;j++){
+			product*=intArr[j];
+		}
+		int product1=product;
+		int a;
+		for(int k=0;k<n;k++){
+			a=product/intArr[k];
+			intArr[k]=a;
+			a=product;
+			System.out.println(intArr[k]);
+		}
+	}
+}
